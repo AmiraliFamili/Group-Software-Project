796d59fa49a4b8f24afe89779931bf62aadf2df3
sebastianroot <126176193+sebastianroot@users.noreply.github.com>
2024-03-21 01:05:28 +0000

    Update admin.py

--- project/game/admin.py
+++ project/game/admin.py
@@ -2,23 +2,27 @@ from django import forms
 from django.contrib import admin
 from .models import UserProfile, Location, Item, Inventory
 
+## Creates a single line to allow gamekeepers to view item positions for each item a user owns.
 class InventoryInlineFormset(forms.BaseInlineFormSet):
     def _construct_form(self, i, **kwargs):
         form = super()._construct_form(i, **kwargs)
         form.fields['position'].required = False
         return form
 
+# Displays the inventory as an inline list for easier viewing and changing.
 class InventoryInline(admin.TabularInline):
     model = Inventory
     formset = InventoryInlineFormset
     extra = 1
 
+# Displays a user's profile with the number of unlocked locations they have as part of the form.
 class UserProfileForm(forms.ModelForm):
     unlocked_locations = forms.ModelMultipleChoiceField(
         queryset = Location.objects.all(),
         required=False
     )
 
+# Combines the previously defined functions into a singularly defined admin
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "countLoc")
     form = UserProfileForm
